ef233e794cfdd7135b00b87f10c82b7fc41103b6
vansh0427 <vatsvansh332@gmail.com>
2026-03-23 22:06:29 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Backend Running");
-});
-
-app.listen(5000, () => console.log("Server running on port 5000"));
